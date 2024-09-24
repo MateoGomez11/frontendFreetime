1ef409fd4c91a6e373af39faae2ec4b855c0a554
MateoGomez11 <mateogomezp11@gmail.com>
2024-09-23 21:07:29 -0500

    Support request, request create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { RegisterComponent } from './seguridad/register/register.component';
 import { FormfreetimerComponent } from './seguridad/form-freetimer/form-freetimer.component';
 import { FormTerminosComponent } from './seguridad/form-terminos/form-terminos.component';
 import { FormBothComponent } from './seguridad/form-both/form-both.component';
+import { SupportComponent } from './support/supportRequest/support.component';
+import { requestComponent } from './support/requestCreate/request.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -12,5 +14,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'form-freetimer', component: FormfreetimerComponent},
   { path: 'form-terminos', component:FormTerminosComponent},
-  { path: 'form-both', component:FormBothComponent}
+  { path: 'form-both', component:FormBothComponent},
+  { path: 'supportrequest', component: SupportComponent},
+  { path: 'requestcreate', component: requestComponent},
 ];
