41b454959b789d77a318f047bd7faaf20af7a753
AndresRojas1704 <andrescamilropa@gmail.com>
2024-09-23 23:13:15 -0500

    hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,9 @@ import { RegisterComponent } from './seguridad/register/register.component';
 import { FormfreetimerComponent } from './seguridad/form-freetimer/form-freetimer.component';
 import { FormTerminosComponent } from './seguridad/form-terminos/form-terminos.component';
 import { FormBothComponent } from './seguridad/form-both/form-both.component';
-import { newTaskComponent } from  './task/newTask1/newTask.component';
-
+import { newTaskComponent } from './task/newTask1/newTask.component';
+import {newTaskComponent3} from './task/newTask3/newTask3.component';
+ 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
@@ -14,5 +15,6 @@ export const routes: Routes = [
   { path: 'form-freetimer', component: FormfreetimerComponent },
   { path: 'form-terminos', component: FormTerminosComponent },
   { path: 'form-both', component: FormBothComponent },
-  { path: 'task', component: newTaskComponent }
+  { path: 'task', component: newTaskComponent },
+  {path: 'newTask3', component: newTaskComponent3}
 ];
