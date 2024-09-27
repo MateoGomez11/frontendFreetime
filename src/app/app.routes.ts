f3a67fc8c920d0f42d0cdb91400b17bdc15f9374
JohnJaider955 <jaidercardona955@gmail.com>
2024-09-27 18:53:59 -0500

    Se actualizó la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegisterComponent } from './seguridad/register/register.component';
 import { FormfreetimerComponent } from './seguridad/form-freetimer/form-freetimer.component';
 import { FormTerminosComponent } from './seguridad/form-terminos/form-terminos.component';
 import { FormBothComponent } from './seguridad/form-both/form-both.component';
+import { AccountVerificationComponent } from './seguridad/account-verification/account-verification.component';
 import { newTaskComponent } from './task/newTask1/newTask.component';
 import { newTaskComponent2 } from './task/newTask2/newTask2.component';
 import { newTaskComponent3 } from './task/newTask3/newTask3.component';
@@ -18,6 +19,7 @@ export const routes: Routes = [
   { path: 'form-freetimer', component: FormfreetimerComponent },
   { path: 'form-terminos', component: FormTerminosComponent },
   { path: 'form-both', component: FormBothComponent },
+  { path: 'account-verification', component: AccountVerificationComponent},
   { path: 'newTask', component: newTaskComponent },
   { path: 'newTask2', component: newTaskComponent2 },
   { path: 'newTask3', component: newTaskComponent3 },
