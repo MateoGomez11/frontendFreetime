e0145e6a90fa41640929027c237fd79a8527e128
JohnJaider955 <jaidercardona955@gmail.com>
2024-11-03 14:35:59 -0500

    Se añadió el diseño de verificación completada

---
+++ src/app/seguridad/successful-verification/successful-verification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-successful-verification',
+  standalone: true,
+  imports: [],
+  templateUrl: './successful-verification.component.html',
+  styleUrl: './successful-verification.component.css'
+})
+export class SuccessfulVerificationComponent {
+
+}
